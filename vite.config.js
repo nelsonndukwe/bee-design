1c3e5a88823ccdf770fe80a395bdfc3ba8c5b81e
nelson <nelsonndukwe800@gmail.com>
2025-11-08 13:27:53 +0100

    Set base path in Vite configuration to ensure assets load from the root in production

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ensures all assets and JS load from the root in production
 })
